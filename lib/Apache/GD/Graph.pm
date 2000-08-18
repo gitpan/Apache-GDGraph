b59c39f3805e3c4bd0ef1ebf53116280358adac3
Rafael Kitover <rkitover@gmail.com>
2000-08-18 00:01:29 +0000

    Import of RKITOVER/Apache-GDGraph-0.4 from CPAN.
    gitpan-cpan-distribution: Apache-GDGraph
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         RKITOVER/Apache-GDGraph-0.4.tar.gz
    gitpan-cpan-author:       RKITOVER
    gitpan-cpan-maturity:     released

--- lib/Apache/GD/Graph.pm
+++ lib/Apache/GD/Graph.pm
@@ -1,6 +1,6 @@
 package Apache::GD::Graph;
 
-($VERSION) = '$ProjectVersion: 0.3 $' =~ /\$ProjectVersion:\s+(\S+)/;
+($VERSION) = '$ProjectVersion: 0.4 $' =~ /\$ProjectVersion:\s+(\S+)/;
 
 =head1 NAME
 
@@ -19,7 +19,7 @@ In httpd.conf:
 
 Then send requests to:
 
-C<http://www.your-server.com/chart?type=lines&x_labels=1st,2nd,3rd,4th,5th&data1=1,2,3,4,5&data2=6,7,8,9,10>
+C<http://www.server.com/chart?type=lines&x_labels=[1st,2nd,3rd,4th,5th]&data1=[1,2,3,4,5]&data2=[6,7,8,9,10]&dclrs=[blue,yellow,green]>
 
 =head1 DESCRIPTION
 
@@ -27,9 +27,8 @@ This is a simple Apache mod_perl handler that generates and returns a png
 format graph based on the arguments passed in via a query string. It responds
 with the content-type "image/png" directly, and sends a Expires: header of 30
 days ahead (since the same query string generates the same graph, they can be
-cached). In addition, it keeps a server-side cache under /tmp/graph-(PID) for
-the lifetime of the Apache process. (These are safe to periodically clean out
-with a cron job).
+cached). In addition, it keeps a server-side cache under
+/var/cache/Apache::GD::Graph .
 
 =head1 OPTIONS
 
@@ -38,19 +37,21 @@ with a cron job).
 =item B<type>
 
 Type of graph to generate, can be lines, bars, points, linespoints, area,
-mixed, pie. For a description of these, see L<GD::Graph(3)>.
+mixed, pie. For a description of these, see L<GD::Graph(3)>. Can also be one of
+the 3d types if GD::Graph3d is installed, or anything else with prefix
+GD::Graph::.
 
 =item B<width>
 
-Width of graph in pixels.
+Width of graph in pixels, 400 by default.
 
 =item B<height>
 
-Height of graph in pixels.
+Height of graph in pixels, 300 by default.
 
 =back
 
-For the following three, look at the plot method in L<GD::Graph(3)>.
+For the following, look at the plot method in L<GD::Graph(3)>.
 
 =over 8
 
@@ -66,7 +67,29 @@ times with N increasing.
 
 =back
 
-ALL OTHER OPTIONS are passed as a hash to the GD::Graph set method.
+ALL OTHER OPTIONS are passed as a hash to the GD::Graph set method using the
+following rules for the values:
+
+=over 8
+
+=item B<undef>
+
+Becomes a real undef.
+
+=item B<[one,two,3]>
+
+Becomes an array reference.
+
+=item B<{one,1,two,2}>
+
+Becomes a hash reference.
+
+=item B<http://somewhere/file.png>
+
+Is pulled into a file and the file name is passed to the respective option.
+(Can be any scheme besides http:// that LWP::Simple supports.)
+
+=back
 
 =cut
 
@@ -74,41 +97,33 @@ use strict;
 use Apache;
 use HTTP::Date;
 use GD::Graph;
-use GD::Graph::lines;
-use GD::Graph::bars;
-use GD::Graph::points;
-use GD::Graph::linespoints;
-use GD::Graph::area;
-use GD::Graph::mixed;
-use GD::Graph::pie;
-use Data::Dumper;
 
 use constant THIRTY_DAYS => 60*60*24*30;
-use constant DIR         => "/tmp/graph-$$/";
+use constant DIR         => "/var/cache/Apache::GD::Graph/";
 
-# Create directory on load, unless this isn't really Apache.
-if (Apache->can('server') && ! -d DIR) {
-	mkdir DIR, 0777;
-	chown Apache->server->uid, Apache->server->gid, DIR;
-}
+BEGIN {
+	my $server = Apache->can('server');
 
-# And delete on process exit.
-END {
-	if (-d DIR) {
-		system "rm -rf ".DIR." 2>&1";
+	if (!-d DIR && $server) {
+		system "mkdir -p ".DIR;
+		chmod 0755, DIR;
+		chown $server->uid, $server->gid, DIR;
 	}
 }
 
 sub handler {
 	my $r = shift;
 	Apache->request($r);
-	my $cached_name = DIR.$r->args.'.png';
+	my $cached_name = $r->args;
+	$cached_name    =~ s|/|\%2f|g;
+	$cached_name    = DIR."/".$cached_name.".png";
 
 	if (-e $cached_name) {
 		local $/ = undef;
 		$r->header_out("Expires" => time2str(time + THIRTY_DAYS));
 		$r->send_http_header("image/png");
-		$r->print((new IO::File $cached_name)->getlines());
+# Slurp the whole thing out.
+		{local (@ARGV,$/) = $cached_name; $r->print(<>)}
 		return 1;
 	}
 
@@ -118,26 +133,54 @@ sub handler {
 	my $width  = delete $args{width}  || 400;
 	my $height = delete $args{height} || 300;
 
-	my $x_labels = [ split /,/, delete $args{x_labels} ]
-		|| [ qw( 1st 2nd 3rd 4th 5th ) ];
-	
+	my $x_labels = parse (delete $args{x_labels})
+		if exists $args{x_labels};
+
 	my @data;
 	my $key = "data1";
 	while (exists $args{$key}) {
-		push @data, [ split /,/, delete $args{$key} ];
+		push @data, parse (delete $args{$key});
 		$key++;
+	}
+
+	return error("Please supply at least a data1 argument.")
+		if ref $data[0] ne 'ARRAY';
+
+	my $length = scalar @{$data[0]};
+
+	return error("data1 empty!") if $length == 0;
+
+# Validate the sizes in order to have a more friendly error.
+	if ( (not defined $x_labels) || scalar @$x_labels == 0) {
+		$x_labels = [1..$length];
+	} elsif (scalar @$x_labels != $length) {
+		return error (
+		 "Size of x_labels not the same as length of data."
+		);
+	}
+
+	my $n = 2;
+	for (@data[1..$#data]) {
+		if (scalar @$_ != $length) {
+			return error (
+			 "Size of data$n not the same as size of data1."
+			);
+		}
+		$n++;
 	}
 
 	my $graph;
 	eval {
 		no strict 'refs';
+		require "GD/Graph/$type.pm";
 		$graph = ('GD::Graph::'.$type)->new($width, $height);
 	}; if ($@) {
-		error (
-		 $r,
+		return error (
 		 "Could not create an instance of class GD::Graph::$type: $@"
 		);
 	}
+
+	$args{$_} = parse ($args{$_}) for (keys %args);
 
 	$graph->set(%args);
 
@@ -147,16 +190,60 @@ sub handler {
 	$r->print($image);
 
 	my $cache = new IO::File ">$cached_name"
-		or error($r, "Could not open $cached_name for writing: $!");
+		or return error("Could not open $cached_name for writing: $!");
 
+	binmode $cache;	# For win32 compatability.
 	print $cache $image;
 	close $cache;
 
 	return 1;
 }
 
-sub error {
-	my ($r, $message) = @_;
+# Parse a datum into a scalar, arrayref or hashref. Using the following semi
+# perl-like syntax:
+#
+# undef		  -- a real undef
+# foo_bar         -- a scalar
+# [1,2,3,foo,bar] -- an array
+# {1,2,3,foo}     -- a hash
+# or
+# http://some/url.png -- pull a URL into a file, returning that.
+sub parse ($) {
+	local $_ = shift;
+
+	if ($_ eq 'undef') {
+		return undef;
+	}
+
+	if (/^\[(.*)\]$/) {
+		return [ split /,/, $1 ];
+	}
+
+	if (/^\{(.*)\}$/) {
+		return { split /,/, $1 };
+	}
+
+	if (m!^\w+://!) {
+		use LWP::Simple;
+
+		my ($url, $file_name) = ($_, $_);
+		$file_name =~ s|/|\%2f|g;
+		$file_name = DIR."/".$file_name;
+
+		my $file = new IO::File "> ".$file_name or
+			error ("Could not open $file_name for writing: $!");
+		binmode $file;
+		print $file get($url);
+		return $file_name;
+	}
+
+	return $_;
+}
+
+# Print an error message.
+sub error ($) {
+	my ($message) = @_;
+	my $r = Apache->request;
 	$r->send_http_header("text/html");
 	$r->print(<<"EOF");
 <html>
@@ -192,6 +279,9 @@ Probably a few.
 
 Configuration of cache dirs, value of the expires header and other options via
 PerlSetEnv directives in httpd.conf.
+
+Perhaps using mod_proxy for caching entirely, or improving this scheme to be
+more intelligent.
 
 Let me know.
 
